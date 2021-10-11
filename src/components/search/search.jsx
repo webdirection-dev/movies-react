45d41a62ec04018cd390ff92be8ba2fbfa8b85ee
MacAir <itech.capitals@icloud.com>
2021-10-11 23:52:22 +0300

    ADD
    Added Search

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,9 +1,52 @@
 import React, {Component} from "react";
+import './search.css'
 
 export default class Search extends Component {
+    state = {
+        search: ''
+    }
+
+    onSearch = (event) => {
+        // получаем данные из импута
+        const {value} = event.target;
+
+        this.setState({
+            search: value
+        })
+    }
+
+    onPushEnter = (event) => {
+        // получить код нажатой клавиши
+        const {code} = event;
+        const buttonClick = event.target.nodeName;
+
+        const {toSearch} = this.props;
+        const search = this.state.search;
+
+        if (code === 'Enter' || buttonClick === 'BUTTON') toSearch(search);
+    }
+
     render() {
+        const {search} = this.state;
+
         return(
-            <h1>Test</h1>
+            <div className="row">
+                <div className="input-field">
+                    <input
+                        className="validate"
+                        placeholder='search'
+                        type="search"
+                        value={search}
+                        onChange={this.onSearch}
+                        onKeyDown={this.onPushEnter}
+                    />
+
+                    <button
+                        className='btn search__btn'
+                        onClick={this.onPushEnter}
+                    >Search</button>
+                </div>
+            </div>
         )
     }
 }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,30 +1,68 @@
 import React, {Component} from "react";
+import Search from "../components/search";
 import MoviesList from "../components/moviesList";
 import Preloader from "../components/preloader/preloader";
 import './layout.css'
 
 export default class Main extends Component {
-    state = {
-        moviesList: null
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            moviesList: null
+        }
+
+        this.getData = this.getData.bind(this);
     }
 
-    async componentDidMount() {
-        const url = 'http://www.omdbapi.com/?apikey=9130863b&s=matrix';
+    componentDidMount() {
+        this.getData('matrix');
+    }
+
+    async getData(str) {
+        const url = `http://www.omdbapi.com/?apikey=9130863b&s=${str}`;
         await fetch(url)
             .then(response => response.json())
             .then(response => {this.setState({moviesList: response.Search})})
             .catch(error => console.error(error));
     }
 
+    toSearch = (str) => {
+        this.getData(str);
+        // мой вариант
+        // const arr = [];
+        //
+        // this.state.moviesList.forEach(item => {
+        //     if (item.Title.includes(str)) {
+        //         arr.push(item)
+        //     }
+        // });
+        //
+        // this.setState({
+        //     moviesList: arr
+        // })
+    }
+
     render() {
         const {moviesList} = this.state;
 
         return(
-            <main className='container content'>
-                {
-                    !moviesList ? <Preloader /> : <MoviesList moviesList={moviesList}/>
-                }
-            </main>
+            <>
+                <main className='container content'>
+                    <div className="main__preloader">
+                        {!moviesList ? <Preloader/> : null}
+                    </div>
+
+                    <Search
+                        toSearch={this.toSearch}
+                        getData={this.getData}
+                    />
+
+                    {
+                        !moviesList ? null : <MoviesList moviesList={moviesList}/>
+                    }
+                </main>
+            </>
         )
     }
 };
